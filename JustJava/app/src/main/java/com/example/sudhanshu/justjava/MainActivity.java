f884ad7048abc991e7bf8dfe227a585d2950fbd7
Sudhanshu Ranjan <perfectsudh@gmail.com>
2017-04-25 14:25:27 +0530

    Spanish support

--- JustJava/app/src/main/java/com/example/sudhanshu/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/sudhanshu/justjava/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.sudhanshu.justjava;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
@@ -25,16 +28,20 @@ public class MainActivity extends AppCompatActivity {
     int quantity = 0;
     boolean whippedCream = false;
     boolean chocolate = false;
+    String name = "";
 
     public void increaseQuantity(View view) {
-        quantity += 1;
+        if (quantity < 100)
+            quantity += 1;
+        else
+            quantity = 100;
         displayQuantity(quantity);
     }
 
     public void decreaseQuantity(View view) {
-        if (quantity > 0)
+        if (quantity > 1)
             quantity -= 1;
-        else quantity = 0;
+        else quantity = 1;
         displayQuantity(quantity);
     }
 
@@ -42,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
         displayQuantity(quantity);
         int price = calculatePrice();
         String message = createOrderSummary(price);
-        displayMessage(message);
+        composeEmail(message);
     }
 
     /**
@@ -54,15 +61,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * This method displays the given price on the screen.
+     * This method displays the order on the screen.
      */
+    /*
     private void displayMessage(String message) {
         TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
         orderSummaryTextView.setText(message);
-    }
-    private void updateCheckBoxes() {
-        CheckBox whippedCheckBox = (CheckBox)findViewById(R.id.whip_cream_checkbox);
-        CheckBox chocolateCheckBox = (CheckBox)findViewById(R.id.chocolate_checkbox);
+    }*/
+    private void updateFields() {
+        CheckBox whippedCheckBox = (CheckBox) findViewById(R.id.whip_cream_checkbox);
+        CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.chocolate_checkbox);
+        EditText editText = (EditText) findViewById(R.id.name_field);
+        name = editText.getText().toString();
         whippedCream = whippedCheckBox.isChecked();
         chocolate = chocolateCheckBox.isChecked();
     }
@@ -70,21 +80,33 @@ public class MainActivity extends AppCompatActivity {
     //Calculate price method
     private int calculatePrice() {
         int price = quantity * 5;
-        updateCheckBoxes();
+        updateFields();
         if (whippedCream)
+            price += 1;
+        if (chocolate)
             price += 2;
-        if(chocolate)
-            price += 3;
         return price;
     }
 
     // Create Order Summary method
     private String createOrderSummary(int price) {
-        String message = "Name: Sudhanshu\n";
-        message = message + "Add whipped cream? " + whippedCream + "\n";
-        message = message + "Add chocolate? " + chocolate + "\n";
-        message = message + "Quantity: " + quantity + "\n";
-        message = message + "Total: $" + price + "\nThank you !";
+        String message = getString(R.string.order_summary_name, name) + "\n";
+        message = message + getString(R.string.addCream) + whippedCream + "\n";
+        message = message + getString(R.string.addChocolate) + chocolate + "\n";
+        message = message + getString(R.string.quantity) + ": " + quantity + "\n";
+        message = message + getString(R.string.total) + ": $" + price + "\n" + getString(R.string.thankyou);
         return message;
+    }
+
+    //Email intent
+    private void composeEmail(String order) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        String subject = getString(R.string.subject,name);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, order);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 }
